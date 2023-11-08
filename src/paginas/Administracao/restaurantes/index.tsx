784c9d79ee1d1eb99c09b7da85039705e1d912d8
Mackenzzy <mackenzzy@gmail.com>
2023-11-07 22:44:30 -0300

    adicionada a função excluir a area administrativa

--- src/paginas/Administracao/restaurantes/index.tsx
+++ src/paginas/Administracao/restaurantes/index.tsx
@@ -1,6 +1,7 @@
-import { Paper, TableBody, Table, TableCell, TableContainer, TableHead, TableRow } from "@mui/material"
+import { Paper, TableBody, Table, TableCell, TableContainer, TableHead, TableRow, Button } from "@mui/material"
 import axios from "axios"
 import { useEffect, useState } from "react"
+import { Link } from "react-router-dom"
 import IRestaurante from "../../../interfaces/IRestaurante"
 
 
@@ -14,18 +15,36 @@ const AdministracaoRestaurtantes = () => {
             })
     }, [])
 
+    const excluir = (restauranteAhSerExcluido: IRestaurante) => {
+        axios.delete(`http://localhost:8000/api/v2/restaurantes/${restauranteAhSerExcluido.id}/`)
+            .then(() => {
+                const listaRestaurante = restaurantes.filter(restaurante => restaurante.id !== restauranteAhSerExcluido.id)
+                setRestaurantes([...listaRestaurante])
+            })
+    }
     return (
         <TableContainer component={Paper}>
             <Table>
                 <TableHead>
                     <TableRow>
                         <TableCell>Nome</TableCell>
+                        <TableCell>Editar</TableCell>
+                        <TableCell>Excluir</TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
                     {restaurantes.map(restaurante =>
                         <TableRow key={restaurante.id}>
                             <TableCell>{restaurante.nome}</TableCell>
+                            <TableCell><Link to={`/admin/restaurantes/${restaurante.id}`} >Editar</Link></TableCell>
+                            <TableCell><Button
+                                variant="outlined"
+                                color="error"
+                                onClick={() => excluir(restaurante)}
+          
+                            >Excluir
+                            </Button>
+                            </TableCell>
                         </TableRow>)}
 
                 </TableBody>
